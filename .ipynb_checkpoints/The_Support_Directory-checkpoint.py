7fea98cc45e36375e115fba568e54355f5c3c55d
Akthar Uzzaman <uzzamanakthar@gmail.com>
2026-04-28 21:56:49 +0100

    I have created my first Python file. This will house all the pages of our app and navigation between them.

---
+++ .ipynb_checkpoints/The_Support_Directory-checkpoint.py
@@ -0,0 +1,14 @@
+# This is the main script for our web-app. In this script, the various pages will be declared and navigation between them will be setup.
+
+# Importing Streamlit. Streamlit will be used to create and host our web-app.
+import streamlit as st
+
+# Defining the pages for our web-app. More pages will be added as needed throughout development.
+# EXAMPLE -> home_page = st.Page("home_page.py", title = "Home", icon = "[EMOJI]")
+
+
+# Navigation between the pages.
+# EXAMPLE -> all_pgs = st.navigation([home_page, Wells_Riley_page, Scn_One_page])
+
+# Running all of the pages.
+# EXAMPLE -> all_pgs.run()
